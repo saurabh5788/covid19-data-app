8cc1db8b16afec15dcac9f769b28a8ed19c7dea3
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-03-30 22:47:26 +0530

    Hibernate properties corrcted

--- src/main/java/com/ssingh/covid19/config/SecurityConfig.java
+++ src/main/java/com/ssingh/covid19/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.ssingh.covid19.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
