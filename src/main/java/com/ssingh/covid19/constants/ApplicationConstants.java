9a6e66a2653dc9aefda6de7f9857f65419d55c72
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-04-08 23:12:18 +0530

    Opening State list

--- src/main/java/com/ssingh/covid19/constants/ApplicationConstants.java
+++ src/main/java/com/ssingh/covid19/constants/ApplicationConstants.java
@@ -20,7 +20,7 @@ public interface ApplicationConstants {
 			// Actuator
 			"/actuator",
 			// Application End points
-			"/user/add", "/user/jwt" };
+			"/user/add", "/user/jwt", "/state/list" };
 
 	String AUTH_REQUEST_HEADER = "Authorization";
 	String JWT_TOKEN_PREFIX = "Bearer ";
