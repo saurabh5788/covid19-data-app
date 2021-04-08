7963606eed538a80caf6e84ee2f1a92a3197b535
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-04-08 18:28:45 +0530

    Enabled CORS Support

--- src/main/java/com/ssingh/covid19/config/WebMvcConfig.java
+++ src/main/java/com/ssingh/covid19/config/WebMvcConfig.java
@@ -1,12 +1,13 @@
 package com.ssingh.covid19.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig {
-	//@Bean
+	@Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
